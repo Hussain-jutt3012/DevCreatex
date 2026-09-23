1f9920f35e584ef9418a84166150e36b3df9132c
Hussain-jutt3012 <hali171825@gmail.com>
2026-09-24 00:43:41 +0500

    Update Providers

--- src/providers/Providers.jsx
+++ src/providers/Providers.jsx
@@ -6,7 +6,7 @@ export default function Providers({ children }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="dark"
+      defaultTheme="light"
       enableSystem
     >
       {children}
